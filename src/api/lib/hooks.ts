873aea20142a78da74e58c494df11c058335ae62
Pavel <pavel.p@didww.com>
2022-07-23 18:19:58 -0400

    Improved file structure

--- src/api/lib/hooks.ts
+++ src/api/lib/hooks.ts
@@ -2,22 +2,24 @@ import { useEffect, useState } from 'react';
 import { getReturnedParamsFromSpotifyAuth } from './helpers';
 
 export const useToken = () => {
-  const [token, setToken] = useState('');
+    const [token, setToken] = useState('');
 
-  useEffect(() => {
-    if (window.location.hash) {
-      //eslint-disable-next-line @typescript-eslint/naming-convention
-      const { access_token, expires_in, token_type } = getReturnedParamsFromSpotifyAuth(window.location.hash);
+    useEffect(() => {
+        if (window.location.hash) {
+            //eslint-disable-next-line @typescript-eslint/naming-convention
+            const { access_token, expires_in, token_type } = getReturnedParamsFromSpotifyAuth(
+                window.location.hash
+            );
 
-      localStorage.clear();
+            localStorage.clear();
 
-      localStorage.setItem('accessToken', access_token);
-      localStorage.setItem('tokenType', token_type);
-      localStorage.setItem('expiresIn', expires_in);
+            localStorage.setItem('accessToken', access_token);
+            localStorage.setItem('tokenType', token_type);
+            localStorage.setItem('expiresIn', expires_in);
 
-      setToken(access_token);
-    }
-  }, []);
+            setToken(access_token);
+        }
+    }, []);
 
-  return token;
+    return token;
 };
